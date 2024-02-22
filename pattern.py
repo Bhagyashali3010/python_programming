52537770f2057e5548a7e00b292ee0192da5f1f9
Bhagyashali3010 <142721197+Bhagyashali3010@users.noreply.github.com>
2024-02-22 17:34:31 +0530

    practicing patterns example in python

---
+++ pattern.py
@@ -0,0 +1,83 @@
+
+# for i in range (4):
+#     for j in range (3):
+                   
+#         print("*", end=' ')
+#     print()
+
+
+# num=1
+# for i in range (4):
+#     for j in range (4):
+#        print (num , end=' ')
+#        num+=1
+#     print()    
+
+
+# num=1
+# for i in range (4):
+#     for j in range (4):
+#        print (num , end=' ')
+#     num+=1
+#     print()
+
+
+# num=1
+# for i in range (4):
+
+#     for j in range (4):
+#         print(num, end=' ')
+#         num+=1
+#     num=1
+#     print()
+
+
+# for i in range(4):
+#     for j in range(4):
+#         print(i+1,end="\t")
+#         i+=1
+#     print()
+
+
+# for i in range (5):
+#     for j in range (5):
+#         if i%2==0:
+#             print ("#", end = " ")
+#         else :
+#             print("@", end=" ")
+#     print()    
+
+# num=1
+# for i in range (3):
+#     for j in range (3):
+#         print (num, end ="\t ")
+#         num+=2
+#     print() 
+
+# num=1 
+# for i in range (3):
+#     for j in range (5):
+#         if num%2!=0:
+#             print (num, end="\t")
+#         num+=1
+#     num=1
+#     print()        
+
+
+
+for i in range (0,5,2):                                
+        for j in range (5):
+            if j%2==0:
+                print (i+1+j, end=" ")
+        print()
+
+# for i in range (0, 13, 4):
+#         for j in range (7):
+#             if j%2==0:
+#                 print(i+1+j, end="\t")
+       
+#         print()          
+
+
+
+    
