65f26e9d182f8268632ccdebc4bd657246086b5b
Bhagyashali3010 <142721197+Bhagyashali3010@users.noreply.github.com>
2024-03-04 17:58:06 +0530

    practicing python examples  again

---
+++ again.py
@@ -0,0 +1,51 @@
+# a=int(input("enter a no:"))
+# if a%2==0:
+#     print("even")
+# else :
+#     print("odd")
+
+
+# b=input("enter character :")
+# c=ord(b)
+# if 64<c<91 or 96<c<123:
+#     print("is alphabet")
+# else :
+#     print("is not")
+
+
+# b=input("enter character :")
+# c=ord(b)
+# if  96<c<123:
+#     if c==97 or c==101 or c==105 or c==111 or c==117:
+#         print("its a vovel")
+#     else :
+#         print("consonent")
+# else :
+#     print("not a character")
+
+
+# num=1
+# while (num<11):
+#     print(num)
+#     num+=1
+
+
+# num=int(input("enter 3 digit no.:"))
+# a=num+10
+# while (num<a):
+#     print(num)
+#     num +=1
+
+
+# num=1
+# while (num<101):
+#     if num %2==0:
+#         print(num)
+#     num+=1     
+
+num=1
+sum=0
+while (num<11):
+    sum+=num
+    num+=1
+print(sum)
