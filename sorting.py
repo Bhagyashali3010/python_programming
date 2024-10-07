e2d8b3312e198f80b48dc47d3611ee16a4f2fc6f
Bhagyashali3010 <142721197+Bhagyashali3010@users.noreply.github.com>
2024-10-07 22:51:30 +0530

    practicing sorting ps

---
+++ sorting.py
@@ -0,0 +1,17 @@
+# n=int(input())
+# arr=[]
+# for i in range (n):
+#     arr.append(int(input()))
+# for i in range (len(arr)-1):
+#     for i in range (len(arr)-1):
+#         if arr[i]>arr[i+1]:
+#           a=arr[i]
+#           arr[i]=arr[i+1]
+#           arr[i+1]=a 
+# for i in range (len(arr)):
+#     print(arr[i])
+
+
+x=8
+y=x^20
+print(y)
